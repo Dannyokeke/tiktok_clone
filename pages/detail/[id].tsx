b313fc769c5b51b0483239a09db35637fa42030d
Danny Okeke <dannyokeke14@gmail.com>
2022-07-18 23:53:18 +0100

    ENVIRONMENT VARIABLES

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import { MdOutlineCancel } from 'react-icons/md';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import axios from 'axios';
 
 const Detail = () => {
   return (
@@ -15,14 +16,8 @@ const Detail = () => {
 
 export const getServerSideProps = async ({
   params: { id },
-}: {
-  params: { id: string };
 }) => {
-  const res = await axios.get(`${BASE_URL}/api/post/${id}`);
-
-  return {
-    props: { postDetails: res.data },
-  };
+  const data = await axios.get(`${BASE_URL}/api/post/${id}`);
 };
 
 export default Detail
--- utils/client.ts
+++ utils/client.ts
@@ -5,5 +5,5 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
-  token: 'skPzLYHEcyvBLvWxAwVOwLAqhNmHuuswbRVE2h25Vm8iC2TretKbH9YqowPCGqe3diNKWDNR8AI3lQCe4b3d8yVx9Wel5Ew2DZipk6ldbAVEEunONfnf13C6sF1CjBCO4d7abgxTxW8w86P8zuNKOohfeWPJAoDCqICDXnYXN4lNeoFwuFXs',
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
