e91a3717ada7ba2b5bb31e81f2c8be776d0e32a4
Danny Okeke <dannyokeke14@gmail.com>
2022-07-18 23:26:39 +0100

    Create [id].tsx

---
+++ pages/detail/[id].tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/router';
+import { GoVerified } from 'react-icons/go';
+import Image from 'next/image';
+import Link from 'next/link';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+
+const Detail = () => {
+  return (
+    <div> Detail</div>
+  )
+}
+
+export const getServerSideProps = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const res = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+  return {
+    props: { postDetails: res.data },
+  };
+};
+
+export default Detail
