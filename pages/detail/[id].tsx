cf802cadd95cf5d4931c00332aa9a28ab83804c9
Danny Okeke <dannyokeke14@gmail.com>
2022-08-01 17:25:54 +0100

    finally

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -8,11 +8,46 @@ import { BsFillPlayFill } from 'react-icons/bs';
 import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 import axios from 'axios';
 import { BASE_URL } from '../../utils';
+import { Video } from '../../types';
+
+interface IProps {
+  postDetails : Video;
+}
 
 
-const Detail = ({ postDetails}) => {
+const Detail = ({ postDetails }: IProps) => {
+  const [post, setPost] = useState(postDetails);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isVideoMuted, setIsVideoMuted] = useState<boolean>(false);
+  const [isPostingComment, setIsPostingComment] = useState<boolean>(false);
+  const [comment, setComment] = useState<string>('');
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
+
+  
+  if(!post) return null
   return (
-    <div> Detail</div>
+    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+      <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
+            <div className='opacity-90 absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+              <p className='cursor-pointer ' onClick={() => router.back()}>
+                <MdOutlineCancel className='text-white text-[35px] hover:opacity-90' />
+              </p>
+            </div>
+            <div className='relative'>
+              <div className='lg:h-[100vh] h-[60vh]'>
+                <video
+                  ref={videoRef}
+                  loop
+                  src={post.video.asset.url}
+                  className=' h-full cursor-pointer'
+                ></video>
+              </div>
+            </div>
+      </div>
+         
+    </div>
   )
 }
 
@@ -21,11 +56,11 @@ export const getServerSideProps = async ({
 }: {
   params: { id:  string },
 }) => {
-  const data = await axios.get(`${BASE_URL}/api/post/${id}`);
+  const data = await axios.get(`${BASE_URL}/api/post/${id}`)
+
+  return {
+    props: { postDetails:data }
+  }
 };
 
-return {
-  props: {postDetails:data}
-}
-
-export default Detail
+export default Detail 
